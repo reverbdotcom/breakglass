4c4100c227681882fda4be1240e4c6e4e8eea209
Erik Benoist <ebenoist@gmail.com>
2020-10-12 11:00:14 -0500

    DRY up tests to make intent clearer

--- __tests__/check_for_branch_protection.test.ts
+++ __tests__/check_for_branch_protection.test.ts
@@ -5,16 +5,35 @@ import { mocked } from 'ts-jest/utils';
 import { checkForBranchProtection } from '../src/check_for_branch_protection';
 import { client } from '../src/github';
 
+const VALID_BRANCH_SETTINGS = {
+  enforce_admins: {
+    enabled: true
+  },
+  required_pull_request_reviews: {
+    dismiss_stale_reviews: true,
+  },
+  required_status_checks: {
+    contexts: [{ name: 'ci-foozles' }],
+  }
+}
+
 describe('::checkForBranchProtection', () => {
+  it('does not open an issue if the configuration is valid', async () => {
+    (client.repos.getBranchProtection as any).mockReturnValue(Promise.resolve({
+      data: {
+        ...VALID_BRANCH_SETTINGS,
+      },
+    }))
+
+    await checkForBranchProtection();
+
+    expect(client.issues.create).not.toHaveBeenCalled();
+  });
+
   it('opens an issue if ci checks are not enabled', async () => {
     (client.repos.getBranchProtection as any).mockReturnValue(Promise.resolve({
       data: {
-        enforce_admins: {
-          enabled: true
-        },
-        required_pull_request_reviews: {
-          dismiss_stale_reviews: true,
-        },
+        ...VALID_BRANCH_SETTINGS,
         required_status_checks: {
           contexts: [],
         }
@@ -34,17 +53,10 @@ describe('::checkForBranchProtection', () => {
   it('opens an issue if rules are not applied to admins', async () => {
     (client.repos.getBranchProtection as any).mockReturnValue(Promise.resolve({
       data: {
+        ...VALID_BRANCH_SETTINGS,
         enforce_admins: {
           enabled: false
         },
-        required_pull_request_reviews: {
-          dismiss_stale_reviews: true,
-        },
-        required_status_checks: {
-          contexts: [{
-            name: 'ci',
-          }],
-        }
       },
     }))
 
@@ -61,15 +73,8 @@ describe('::checkForBranchProtection', () => {
   it('opens an issue if reviews are not required', async () => {
     (client.repos.getBranchProtection as any).mockReturnValue(Promise.resolve({
       data: {
-        enforce_admins: {
-          enabled: true
-        },
+        ...VALID_BRANCH_SETTINGS,
         required_pull_request_reviews: null,
-        required_status_checks: {
-          contexts: [{
-            name: 'ci',
-          }],
-        }
       },
     }))
 
@@ -86,17 +91,10 @@ describe('::checkForBranchProtection', () => {
   it('opens an issue if changes do not dismiss a review', async () => {
     (client.repos.getBranchProtection as any).mockReturnValue(Promise.resolve({
       data: {
-        enforce_admins: {
-          enabled: true
-        },
+        ...VALID_BRANCH_SETTINGS,
         required_pull_request_reviews: {
           dismiss_stale_reviews: false,
         },
-        required_status_checks: {
-          contexts: [{
-            name: 'ci',
-          }],
-        }
       },
     }))
 
